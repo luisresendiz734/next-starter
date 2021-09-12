dbc7eb455eff7c035bc4e71a5cefe07b8f639a39
luisresendiz734 <luisresendiz734@gmail.com>
2021-09-12 13:54:09 -0500

    PWA

---
+++ pages/_document.tsx
@@ -0,0 +1,33 @@
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
+
+class AppDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+
+  render() {
+    return (
+      <Html lang="es-MX">
+        <Head>
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/apple-touch-icon.png"></link>
+          <meta name="theme-color" content="#fff" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default AppDocument;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 const Home = () => {
   return (
     <div>
-      <h1>Next.js starter</h1>
-      <ul>
-        <li>Typescript</li>
-        <li>ESLint</li>
-        <li>Prettier</li>
-        <li>Tailwind CSS</li>
+      <h1 className="m-2 font-bold">Next.js starter</h1>
+      <ul className="list-disc">
+        <li className="ml-8">Typescript</li>
+        <li className="ml-8">ESLint</li>
+        <li className="ml-8">Prettier</li>
+        <li className="ml-8">Tailwind CSS</li>
+        <li className="ml-8">PWA</li>
       </ul>
     </div>
   );
