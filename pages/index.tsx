5d5300f3c67dba8b7c7f3573faf87d7d56776b5e
luisresendiz734 <luisresendiz734@gmail.com>
2021-09-12 13:26:22 -0500

    Tailwind css

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,9 @@ const Home = () => {
       <h1>Next.js starter</h1>
       <ul>
         <li>Typescript</li>
+        <li>ESLint</li>
+        <li>Prettier</li>
+        <li>Tailwind CSS</li>
       </ul>
     </div>
   );
