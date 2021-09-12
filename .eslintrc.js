d3a04eaf85fdc712eda92ddb39034c3bf28f2f54
luisresendiz734 <luisresendiz734@gmail.com>
2021-09-12 13:13:59 -0500

    pretify v1

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,23 +3,15 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'prettier'
-  ],
-  parser: '@typescript-eslint/parser',
+  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
-  rules: {
-  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {},
 };
